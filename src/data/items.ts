6b6a951e49935ed276d475b1718a32a4de3434df
jader <jaderluiz01@gmail.com>
2022-06-23 20:43:59 -0300

    estilizaçao
    estilo do projeto e comentarios

---
+++ src/data/items.ts
@@ -0,0 +1,6 @@
+export const itens = [
+    { date: new Date(2022, 9, 15), category: "food", title: "McDonalds", value: 32.25 },
+    { date: new Date(2022, 9, 17), category: "food", title: "Burguer King", value: 50 },
+    { date: new Date(2022, 9, 16), category: "rent", title: "Aluguel Apt", value: 1500 },
+    { date: new Date(2022, 10, 1), category: "salary", title: "Salario ACME", value: 9000 }
+]
